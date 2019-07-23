8a6185270a3659920ce1f6a8c4ece593e91be6f6
Black Jack <hibbarcd@gmail.com>
2019-07-23 10:22:44 -0500

    scss added on menu items

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom'
 import Homepage from './pages/homepage/homepage.component'
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+)
+
+
 
 function App() {
   return (
+    <Switch>
     <div className="App">
         <Homepage />
+        <Route exact path='/' component={Homepage}/>
+        <Route path='hats' component={HatsPage}/>
+
     </div>
+    </Switch>
   );
 }
 
