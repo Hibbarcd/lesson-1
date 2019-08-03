db6d57b3125836ca6d102dbfd2273f21d1767912
Black Jack <hibbarcd@gmail.com>
2019-08-03 08:59:20 -0500

    finished, fixed sign in component to work properly, synced to firebase fully

--- src/pages/signIn-signup/signin-signup.component.jsx
+++ src/pages/signIn-signup/signin-signup.component.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import SignIn from '../../components/sign-in/sign-in.component'
+import SignUp from '../../components/sign-up/sign-up.component'
+
 
 import './signin-signup.style.scss'
 
 const SignUpAndSignInPage = () => (
-    <div className='sign-in-and-sign-up'><SignIn /></div>
+    <div className='sign-in-and-sign-up'>
+    <SignIn />
+    <SignUp />
+    </div>
 )
 export default SignUpAndSignInPage
