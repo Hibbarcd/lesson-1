62be4d97dce7762e5cdc53b738152cb3c14756f1
Black Jack <hibbarcd@gmail.com>
2019-08-02 21:59:31 -0500

    added signup component, not yet imported into app.js

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,14 +11,49 @@ var Config = {
   messagingSenderId: "543241214962",
   appId: "1:543241214962:web:80399c77b16f783d"
 };
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`)
+  const snapShot = await userRef.get()
+  // console.log(snapShot)
+
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+}
 // Initialize Firebase
 firebase.initializeApp(Config);
-
+//OAuth for google sign in on firebase------------------------------------------//
   export const auth = firebase.auth()
-  export const firestore = firebase.firestore()
 
   const provider = new firebase.auth.GoogleAuthProvider();
   provider.setCustomParameters({ prompt: 'select_account' });
   export const signInWithGoogle = () => auth.signInWithPopup(provider);
+  //------------------------------------------------------------------------------//
+
+  export const firestore = firebase.firestore()
+// firestore.collection('users').doc('ACe12L2Suv6x6mhXQz3m').collection('cartItems').doc('fxHDid46e1v6Q666VEgo')
+//=====Identical way of doing the above code in less verbose form
+// firestore.doc('/users/ACe12L2Suv6x6mhXQz3m/cartItems/fxHDid46e1v6Q666VEgo')
+
+//======below is way to grab collection of items within cartItems
+// firestore.collection('/users/ACe12L2Suv6x6mhXQz3m/cartItems/')
+
 
   export default firebase
