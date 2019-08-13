434f04431cbe9da3d05ba6331b3ba22c703242f0
Black Jack <hibbarcd@gmail.com>
2019-08-13 16:39:54 -0500

    reducer for cart dropdown added, hidden component state, dropdown menu functional

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
-
+import  CartIcon  from '../cart-icon/cart-icon.component'
+import CartDropdown from '../cart-dropdown/cart-dropdown.component'
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 import './header.style.scss'
 
 import { auth } from '../../firebase/firebase.utils'
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
     <div className='header'>
             <Link className= 'logo-container' to='/'></Link>
             <Logo className= 'logo' />
@@ -24,11 +25,17 @@ const Header = ({ currentUser }) => (
                 :
                 <Link className='option' to='/signin'>Sign In</Link>
             }
+            <CartIcon />
         </div>
+
+        {  hidden ? null : <CartDropdown />  }
+
     </div>
 )
-const mapStateToProps = state =>  ({
-    currentUser: state.user.currentUser
+
+const mapStateToProps = ({user: { currentUser }, cart:{ hidden } }) =>  ({
+    currentUser,
+    hidden
 })
 
 export default connect(mapStateToProps)(Header)
