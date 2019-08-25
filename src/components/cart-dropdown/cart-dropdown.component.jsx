e3eb41ef017d777311711b7879f472ec06696737
Black Jack <hibbarcd@gmail.com>
2019-08-24 19:50:38 -0500

    cart icon counter added with cart selector

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,13 +1,24 @@
 
 import React from 'react'
+import {connect} from 'react-redux'
 import  './cart-dropdown.style.scss'
+import { selectCartItems} from '../../redux/cart/cart.selectors'
+import CartItem from '../cart-item/cart-item.component'
 
 import CustomButton from '../custom-button/custom-button.component'
 
-const CartDropdown = () => (
+const CartDropdown = ({cartItems}) => (
     <div className='cart-dropdown'>
-        <div className='cart-item' />
+        <div className='cart-item'>
+
+            {cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem} />)}
+
+        </div>
         <CustomButton>Go To Checkout</CustomButton>
     </div>
 )
-export default CartDropdown
+const mapStateToProps = state => ({
+    cartItems: selectCartItems(state) 
+})
+
+export default connect(mapStateToProps)(CartDropdown)
