2e31a531a750d0dfcb011e2525337f6b5b74d935
Black Jack <hibbarcd@gmail.com>
2019-08-27 14:10:33 -0500

    added checkout item component, basic functionality

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -6,10 +6,10 @@ import { withRouter } from 'react-router-dom'
 import { createStructuredSelector } from 'reselect'
 import { selectCartItems} from '../../redux/cart/cart.selectors'
 import CartItem from '../cart-item/cart-item.component'
-
+import { toggleCartHidden } from '../../redux/cart/cart.actions'
 import CustomButton from '../custom-button/custom-button.component'
 
-const CartDropdown = ({cartItems, history}) => (
+const CartDropdown = ({cartItems, history, dispatch }) => (
     <div className='cart-dropdown'>
         <div className='cart-items'>
         {
@@ -21,12 +21,15 @@ const CartDropdown = ({cartItems, history}) => (
                 ))
                 :
                 <span className='empty-message'>Your Cart is Empty</span>
-        }
-        
+        } 
         </div>
-
-        <CustomButton onClick={()=> history.push('/checkout')}>Go To Checkout</CustomButton>
-    </div>
+            <CustomButton onClick={()=> {
+                history.push('/checkout')
+                dispatch(toggleCartHidden())
+                }}>
+                Go To Checkout
+                </CustomButton>
+        </div>
 )
 const mapStateToProps = state => createStructuredSelector({
     cartItems: selectCartItems,
--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors'
+import CheckoutItem from '../../components/checkout-item/checkout-item.component'
 
 import './checkout.style.scss'
 
@@ -25,7 +26,9 @@ const CheckoutPage = ({cartItems, total}) => (
                 <span>REMOVE</span>
             </div>
         </div>
-        {cartItems.map(cartItem => cartItem.name
+        {cartItems.map(cartItem => 
+            <CheckoutItem key={cartItem.id} cartItem={cartItem}
+            />
 
         )}
         <div className='total'>
