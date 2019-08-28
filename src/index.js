4afcd1866fd743d15e078b86cf8437305dd47ee3
Black Jack <hibbarcd@gmail.com>
2019-08-27 21:45:53 -0500

    redux-persist local storage capacity added to cart items for users

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter} from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './redux/store/store'
+import { store, persistor } from './redux/store/store'
+import { persistGate } from 'redux-persist/integration/react'
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
     <Provider store={store}>
-        <BrowserRouter > 
-            <App />
+        <BrowserRouter >
+            <persistGate persistor={persistor} > 
+                <App />
+            </ persistGate >
         </BrowserRouter>
     </Provider>,
  document.getElementById('root'));
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,21 @@
 import { combineReducers } from 'redux'
+import { persistReducer} from 'redux-persist'
+
+import storage from 'redux-persist/lib/storage'
+
 import userReducer from './user/user.reducer.js'
 import cartReducer from './cart/cart.reducer'
 
-export default combineReducers({
+const persistConfig = {
+    key: 'root',
+    storage,
+    //whiteList means only cart is persisted, not the user since that is handled by firebase
+    whiteList: ['cart']
+}
+const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer,
 })
+
+
+export default persistReducer(persistConfig, rootReducer)
