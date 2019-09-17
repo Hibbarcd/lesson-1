f33208042c3fa7c2a1f85456d6b845da71db4161
Black Jack <hibbarcd@gmail.com>
2019-09-17 18:00:10 -0500

    styling of page, stripe api UI upgrade

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -4,6 +4,7 @@ import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors'
 import CheckoutItem from '../../components/checkout-item/checkout-item.component'
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component'
 
 import './checkout.style.scss'
 
@@ -31,9 +32,13 @@ const CheckoutPage = ({cartItems, total}) => (
             />
 
         )}
-        <div className='total'>
-            <span>TOTAL: ${total}</span>
+        <div className='total'>TOTAL: ${total}</div>
+        <div className='test-warning'>
+        *Please use the following credit card test for payment:
+        <br />
+        4242 4242 4242 4242 expiring 01/20 CVV 123*
         </div>
+        <StripeCheckoutButton price={total} />
     </div>
 )
 const mapStateToProps = createStructuredSelector({
